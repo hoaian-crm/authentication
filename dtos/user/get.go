9a36d0aa44c17202e7b354fe5a95fc371b9eda26
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-10-26 16:50:37 +0700

    DisplayName

--- dtos/user/get.go
+++ dtos/user/get.go
@@ -1,7 +1,27 @@
 package user_dto
 
-import "main/dtos"
+import (
+	"main/dtos"
+)
 
 type ListUserDto struct {
 	dtos.Query
+	DisplayName string `json:"displayName" form:"displayName"`
+	Email       string `json:"email" form:"email"`
+}
+
+func (query *ListUserDto) SetDefaults() {
+	query.Query.SetDefaults()
+
+	if query.DisplayName == "" && query.Email {
+		return
+	}
+
+	if query.DisplayName == "" {
+		query.DisplayName = "/"
+	}
+	if query.Email == "" {
+		query.Email = "/"
+	}
+
 }
